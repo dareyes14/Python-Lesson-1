2fe0a316a3727c952ebcd1c87ad446d2a2f64a3a
Reyes <dareyes14@cps.k12.il.us>
2024-09-03 12:20:29 -0500

    first commit

---
+++ lesson1.py
@@ -0,0 +1,35 @@
+# this is the print statement
+# print statement allows us to print out the statement
+#comments are not executed by the computer
+#comments are used to help us understand the code
+# print("Hello my name is Daniel")
+# print("I am a student at JHCP")
+# print("I am learning pythong")
+#create input for the user 
+name=input("What is your name?")
+print("Hello, name")
+#inputs allows us to take input from the user
+
+#ask the user for their age
+#print out the age 
+#ask the user for their favorite color
+#print out the favorite color 
+#ask the user for their favorite food 
+#print out the favorite food 
+
+age=input("How old are you?")
+print("age")
+
+favorite_color=input("What is your favorite color?")
+print("favorite_color")
+
+favorite_food=input("What is your favorite food?")
+print("favorite_food")
+
+#concatenation is the process of combining strings with
+#variables
+# print("You are", age, "years old")
+
+favorite_animal=input("what is your Favorite animal?")
+print("Your favorite animal is a"+ favorite_animal)
+#the plus sign is used concatenate strings
